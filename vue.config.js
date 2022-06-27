60a5b11cd78b45dd1ba4a27e8fcf8d2ebd4884a0
edji777 <yo-suke@mail.ru>
2022-06-27 11:45:07 +0300

    add initial config setup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/scss/fonts.scss";
+        @import "@/assets/scss/mixins.scss";
+        @import "@/assets/scss/variables.scss";
+      `,
+      },
+    },
+  },
 });
